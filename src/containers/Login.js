0860f7354e7013cb70d597397cf7718876c46dbe
Krishna <Krish875076@gmail.com>
2021-05-08 01:01:42 +0530

    done

---
+++ src/containers/Login.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Component } from "react";
+import "./index.css";
+
+
+
+function Login() {
+
+  return (
+    <div className="login">
+      <div className="login-box">
+        <form>
+          <h1>Login</h1>
+          <p>Sign In to your account</p>
+          
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
