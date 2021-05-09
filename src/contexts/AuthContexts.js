e543e82d990a677d8acc53c7dce491988d9bc270
satish0 <satishkumar180495@gmail.com>
2021-05-09 18:23:34 +0530

    updated and dashboard files created

--- src/contexts/AuthContexts.js
+++ src/contexts/AuthContexts.js
@@ -9,27 +9,52 @@ export function useAuth() {
 
 export  function AuthProvider({ children }) {
 
-const [currentUser, setCurrentUser] = useState();  
-const [ loading, setLoading ] = useState(true);
+    const [currentUser, setCurrentUser] = useState();  
+    const [ loading, setLoading ] = useState(true);
 
-function signup(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password)
-}
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password)
+    }
 
-useEffect(()=>{
-    const unsubscribe = auth.onAuthStateChanged(user => {
-        setCurrentUser(user);
-        setLoading(false);
-    })
-    return unsubscribe;
-}, [])
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password)
+    }
+
+    function logout() {
+        return auth.signOut()
+    }
+
+    function resetPassword(email) {
+        return auth.sendPasswordResetEmail(email)
+    }
+
+    function updateEmail(email) {
+        return currentUser.updateEmail(email)
+    }
+
+    function updatePassword(password) {
+        return currentUser.updatePassword(password)
+    }
+
+    useEffect(()=>{
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user);
+            setLoading(false);
+        })
+        return unsubscribe;
+    }, [])
 
 
 
-const value = {
-    currentUser,
-    signup
-}
+    const value = {
+        currentUser,
+        login,
+        signup,
+        logout,
+        resetPassword,
+        updateEmail,
+        updatePassword
+    }
 
     return (
         <AuthContext.Provider value={value}>
