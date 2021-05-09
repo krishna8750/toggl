e0daaae8a4957a527c73c9062aa18c8d9c9f9cc5
satish0 <satishkumar180495@gmail.com>
2021-05-09 22:20:15 +0530

    line added just 2

--- src/contexts/AuthContexts.js
+++ src/contexts/AuthContexts.js
@@ -7,30 +7,30 @@ export function useAuth() {
     return useContext(AuthContext);
 }
 
-export  function AuthProvider({ children }) {
+export function AuthProvider({ children }) {
 
-const [currentUser, setCurrentUser] = useState();  
-const [ loading, setLoading ] = useState(true);
+    const [ currentUser, setCurrentUser ] = useState();  
+    const [ loading, setLoading ] = useState(true);
 
-function signup(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password)
-}
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password)
+    }
 
-useEffect(()=>{
-    const unsubscribe = auth.onAuthStateChanged(user => {
-        setCurrentUser(user);
-        setLoading(false);
-    })
-    
-    return unsubscribe;
-}, [])
+    useEffect(()=>{
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user);
+            setLoading(false);
+        })
+
+        return unsubscribe;
+    }, [])
 
 
 
-const value = {
-    currentUser,
-    signup
-}
+    const value = {
+        currentUser,
+        signup
+    }
 
     return (
         <AuthContext.Provider value={value}>
