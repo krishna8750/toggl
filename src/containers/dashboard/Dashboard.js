320fa1f7c0c9ac787a72f89f48eb9bbe5dfcbe85
Krishna <Krish875076@gmail.com>
2021-05-16 23:28:33 +0530

    done

--- src/containers/dashboard/Dashboard.js
+++ src/containers/dashboard/Dashboard.js
@@ -1,106 +1,185 @@
-import React, { useState } from 'react'
-import { Card, Button, Alert } from 'react-bootstrap'
-import { useAuth } from '../../contexts/AuthContexts'
-import { Link, useHistory } from 'react-router-dom'
-import Sidebar from './Sidebar'
-import './Dashboard.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBell, faFileAlt, faFileContract, faFolder, faUserTie, faUsers, faUserCog, faHandsHelping, faUserCircle, faPlayCircle } from '@fortawesome/free-solid-svg-icons'
+import React, { useEffect, useState } from "react";
+import { Card, Button, Alert } from "react-bootstrap";
+import { useAuth, currentUser } from "../../contexts/AuthContexts";
+import { Link, useHistory } from "react-router-dom";
+import { db } from "../../firebase";
+import Sidebar from "./Sidebar";
+import "./Dashboard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBell,
+  faFileAlt,
+  faFileContract,
+  faFolder,
+  faUserTie,
+  faUsers,
+  faUserCog,
+  faHandsHelping,
+  faUserCircle,
+  faPlayCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Dashboard() {
-    const [error, setError] = useState("")
-    const { currentUser, logout } = useAuth()
-    const history = useHistory()
+  const [error, setError] = useState("");
+  const [userName, setUserName] = useState("");
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
 
-    async function handleLogout() {
-        setError("")
+  async function handleLogout() {
+    setError("");
 
-        try {
-            await logout()
-            history.push("/login")
-        } catch {
-            setError("Failed to Logout")
-        }
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("Failed to Logout");
     }
+  }
 
-    return (
-        <>
-            <section className="dashboard">
-                <div className="left-col left-nav">
-                    <div className="sidebar">
-                        <div className="sidebar-inner">
-                            <ul className="logo">
-                                <li><span>Toggl</span><FontAwesomeIcon icon={faBell} /></li>
-                            </ul>
-                            <div className="common-menu reports mt-4">
-                                <p className="sub-heading">Analyze</p>
-                                <ul className="list-menu">
-                                    <li><a href="javascript:;"><FontAwesomeIcon icon={faFileAlt} /><span>Reports</span></a></li>
-                                    <li><a href="javascript:;"><FontAwesomeIcon icon={faFileContract} /><span>Insights</span></a></li>
-                                </ul>
-                            </div>
-                            <div className="common-menu manage mt-4">
-                                <p className="sub-heading">Manage</p>
-                                <ul className="list-menu">
-                                    <li><a href="javascript:;"><FontAwesomeIcon icon={faFolder} /><span>Projects</span></a></li>
-                                    <li><a href="javascript:;"><FontAwesomeIcon icon={faUserTie} /><span>Clients</span></a></li>
-                                    <li><a href="javascript:;"><FontAwesomeIcon icon={faUsers} /><span>Teams</span></a></li>
-                                    <li><a href="javascript:;"><FontAwesomeIcon icon={faUserCog} /><span>Settings</span></a></li>
-                                    <li><a href="javascript:;"><FontAwesomeIcon icon={faHandsHelping} /><span>Helps</span></a></li>
-                                </ul>
-                            </div>
-                            <div className="common-menu user-menu mt-4">
-                                <p className="sub-heading">Workspace</p>
-                                <ul className="list-menu">
-                                    <li><a href="javascript:;"><span><span>Satish Kumar</span><span>satishkumar180495@gmail.com</span></span><FontAwesomeIcon icon={faUserCircle} /></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+  const ref = db.collection("users");
+     ref.get().then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        if (doc.id == currentUser.uid) {
+          console.log(doc.id);
+          console.log(doc.data().uName);
+          setUserName(doc.data().uName);
+        }
+      });
+    });
+  
+
+  return (
+    <>
+      <section className="dashboard">
+        <div className="left-col left-nav">
+          <div className="sidebar">
+            <div className="sidebar-inner">
+              <ul className="logo">
+                <li>
+                  <span>Toggl</span>
+                  <FontAwesomeIcon icon={faBell} />
+                </li>
+              </ul>
+              <div className="common-menu reports mt-4">
+                <p className="sub-heading">Analyze</p>
+                <ul className="list-menu">
+                  <li>
+                    <a href="javascript:;">
+                      <FontAwesomeIcon icon={faFileAlt} />
+                      <span>Reports</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="javascript:;">
+                      <FontAwesomeIcon icon={faFileContract} />
+                      <span>Insights</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="common-menu manage mt-4">
+                <p className="sub-heading">Manage</p>
+                <ul className="list-menu">
+                  <li>
+                    <a href="javascript:;">
+                      <FontAwesomeIcon icon={faFolder} />
+                      <span>Projects</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="javascript:;">
+                      <FontAwesomeIcon icon={faUserTie} />
+                      <span>Clients</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="javascript:;">
+                      <FontAwesomeIcon icon={faUsers} />
+                      <span>Teams</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="javascript:;">
+                      <FontAwesomeIcon icon={faUserCog} />
+                      <span>Settings</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="javascript:;">
+                      <FontAwesomeIcon icon={faHandsHelping} />
+                      <span>Helps</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="common-menu user-menu mt-4">
+                <p className="sub-heading">Workspace</p>
+                <ul className="list-menu">
+                  <li>
+                    <a href="javascript:;">
+                      <span>
+                        <span>{userName}</span>
+                        <span>{currentUser.email}</span>
+                      </span>
+                      <FontAwesomeIcon icon={faUserCircle} />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right-col right-dashboard">
+          <div className="dashboard-content">
+            <div className="add-task">
+              <form action="">
+                <input type="text" placeholder="What are you working on?" />
+                <div className="action-option">
+                  <div className="action">
+                    <FontAwesomeIcon icon={faFolder} />
+                  </div>
+                  <div className="action timer">
+                    <input type="text" placeholder="0:00:00" />
+                  </div>
+                  <div className="action play">
+                    <button>
+                      <FontAwesomeIcon icon={faPlayCircle} />
+                    </button>
+                  </div>
                 </div>
-                <div className="right-col right-dashboard">
-                    <div className="dashboard-content">
-                        <div className="add-task">
-                            <form action="">
-                                <input type="text" placeholder="What are you working on?" />
-                                <div className="action-option">
-                                    <div className="action">
-                                        <FontAwesomeIcon icon={faFolder} />
-                                    </div>
-                                    <div className="action timer">
-                                        <input type="text" placeholder="0:00:00" />
-                                    </div>
-                                    <div className="action play">
-                                        <button><FontAwesomeIcon icon={faPlayCircle} /></button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <div className="task-list">
-                            <div className="this-week">
-                                <p>This week 0:00:00</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="container-fluid mt-5">
-                        <div className="row">
-                            <div className="col-12">
-                                <Card>
-                                    <Card.Body>
-                                        <h1>Dash Board</h1>
-                                        { error && <Alert variant="danger">{error}</Alert> }
-                                        <h2><strong>Email: </strong> {currentUser.email}</h2>
-                                        <Link to="/updateprofile" className="btn btn-primary mt-2">Update Profile</Link>
-                                    </Card.Body>
-                                </Card>
-                                <div className="w-100 mt-2">
-                                    <Button onClick={handleLogout} className="btn btn-danger">Log Out</Button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+              </form>
+            </div>
+            <div className="task-list">
+              <div className="this-week">
+                <p>This week 0:00:00</p>
+              </div>
+            </div>
+          </div>
+          <div className="container-fluid mt-5">
+            <div className="row">
+              <div className="col-12">
+                <Card>
+                  <Card.Body>
+                    <h1>Dash Board</h1>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <h2>
+                      <strong>Email: </strong> {currentUser.email}
+                    </h2>
+                    <Link to="/updateprofile" className="btn btn-primary mt-2">
+                      Update Profile
+                    </Link>
+                  </Card.Body>
+                </Card>
+                <div className="w-100 mt-2">
+                  <Button onClick={handleLogout} className="btn btn-danger">
+                    Log Out
+                  </Button>
                 </div>
-            </section>
-        </>
-    )
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/contexts/AuthContexts.js
+++ src/contexts/AuthContexts.js
@@ -1,68 +1,68 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { auth, db } from "../firebase"
+import React, { useContext, useState, useEffect } from "react";
+import { auth, db } from "../firebase";
 
 const AuthContext = React.createContext();
 
 export function useAuth() {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 }
 
 export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
 
-    const [currentUser, setCurrentUser] = useState();  
-    const [ loading, setLoading ] = useState(true);
+  async function signup(email, password, userName) {
+    return auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+      return db.collection("users").doc(cred.user.uid).set({
+        uName: userName,
+      });
+    });
+  }
 
-    async function signup(email, password, userName) {
-        return auth.createUserWithEmailAndPassword(email, password).then(cred => {
-            return db.collection('users').doc(cred.user.uid).set({
-                uName: userName
-            });
-        });
-    };
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
 
-    function login(email, password) {
-        return auth.signInWithEmailAndPassword(email, password)
-    }
+  function logout() {
+    return auth.signOut();
+  }
 
-    function logout() {
-        return auth.signOut()
-    }
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
 
-    function resetPassword(email) {
-        return auth.sendPasswordResetEmail(email)
-    }
+  function updateEmail(email) {
+    return currentUser.updateEmail(email);
+  }
 
-    function updateEmail(email) {
-        return currentUser.updateEmail(email)
-    }
-
-    function updatePassword(password) {
-        return currentUser.updatePassword(password)
-    }
-
-    useEffect(()=>{
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            setCurrentUser(user);
-            setLoading(false);
-        })
-        return unsubscribe;
-    }, [])
+  function updatePassword(password) {
+    return currentUser.updatePassword(password);
+  }
 
 
 
-    const value = {
-        currentUser,
-        login,
-        signup,
-        logout,
-        resetPassword,
-        updateEmail,
-        updatePassword
-    }
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      console.log("first");
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
 
-    return (
-        <AuthContext.Provider value={value}>
-            { !loading && children}
-        </AuthContext.Provider>
-    )
+  const value = {
+    currentUser,
+    login,
+    signup,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
